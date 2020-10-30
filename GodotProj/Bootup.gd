6aeddec2c6acd308481dbfc6d2c0c20f08a54671
Jake <haekbreen@gmail.com>
2020-10-29 22:42:29 -0700

    Init commit!

---
+++ GodotProj/Bootup.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# load the Simple library
+onready var data = preload("res://SimpleLibCaller.gdns").new()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print ( data.method('hi') )
+	data.test_void_method()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
