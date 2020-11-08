7174b1ebd75f14b62634c31a2bf9651005b70e29
Jake <haekbreen@gmail.com>
2020-11-08 14:32:24 -0800

    Adjust CSBase to be a proper abstract class. (This might break some things...)

--- LTEL/LTEL/LT1/AppHeaders/CSBase.h
+++ LTEL/LTEL/LT1/AppHeaders/CSBase.h
@@ -18,76 +18,76 @@
 		// WriteToMessageHMessageWrite to write it into the other HMESSAGE, then call EndMessageHMessageWrite to free it.
 		virtual HMESSAGEWRITE	StartHMessageWrite()=0;
 		
-		virtual DRESULT			WriteToMessageFloat(HMESSAGEWRITE hMessage, float val);
-		virtual DRESULT			WriteToMessageByte(HMESSAGEWRITE hMessage, DBYTE val);
-		virtual DRESULT			WriteToMessageWord(HMESSAGEWRITE hMessage, D_WORD val);
-		virtual DRESULT			WriteToMessageDWord(HMESSAGEWRITE hMessage, DDWORD val);
-		virtual DRESULT			WriteToMessageString(HMESSAGEWRITE hMessage, char *pStr);
-		virtual DRESULT			WriteToMessageVector(HMESSAGEWRITE hMessage, DVector *pVal); // 12 bytes
-		virtual DRESULT			WriteToMessageCompVector(HMESSAGEWRITE hMessage, DVector *pVal); // 9 bytes
-		virtual DRESULT			WriteToMessageCompPosition(HMESSAGEWRITE hMessage, DVector *pVal); // 7 bytes
-		virtual DRESULT			WriteToMessageRotation(HMESSAGEWRITE hMessage, DRotation *pVal);
-		virtual DRESULT			WriteToMessageHString(HMESSAGEWRITE hMessage, HSTRING hString);
+		virtual DRESULT			WriteToMessageFloat(HMESSAGEWRITE hMessage, float val) = 0;
+		virtual DRESULT			WriteToMessageByte(HMESSAGEWRITE hMessage, DBYTE val) = 0;
+		virtual DRESULT			WriteToMessageWord(HMESSAGEWRITE hMessage, D_WORD val) = 0;
+		virtual DRESULT			WriteToMessageDWord(HMESSAGEWRITE hMessage, DDWORD val) = 0;
+		virtual DRESULT			WriteToMessageString(HMESSAGEWRITE hMessage, char *pStr) = 0;
+		virtual DRESULT			WriteToMessageVector(HMESSAGEWRITE hMessage, DVector *pVal) = 0; // 12 bytes
+		virtual DRESULT			WriteToMessageCompVector(HMESSAGEWRITE hMessage, DVector *pVal) = 0; // 9 bytes
+		virtual DRESULT			WriteToMessageCompPosition(HMESSAGEWRITE hMessage, DVector *pVal) = 0; // 7 bytes
+		virtual DRESULT			WriteToMessageRotation(HMESSAGEWRITE hMessage, DRotation *pVal) = 0;
+		virtual DRESULT			WriteToMessageHString(HMESSAGEWRITE hMessage, HSTRING hString) = 0;
 
 		// Writes a HMESSAGEWRITE into an already opened HMESSAGEWRITE.
 		// Inputs:
 		//		hMessage -		HMESSAGEWRITE written to.
 		//		hDataMessage -	HMESSAGEWRITE written from.
-		virtual DRESULT			WriteToMessageHMessageWrite(HMESSAGEWRITE hMessage, HMESSAGEWRITE hDataMessage);
+		virtual DRESULT			WriteToMessageHMessageWrite(HMESSAGEWRITE hMessage, HMESSAGEWRITE hDataMessage) = 0;
 		
 		// Writes a HMESSAGEREAD into an already opened HMESSAGEWRITE.
 		// Inputs:
 		//		hMessage -		HMESSAGEWRITE written to.
 		//		hDataMessage -	HMESSAGEREAD written from.
-		virtual DRESULT			WriteToMessageHMessageRead(HMESSAGEWRITE hMessage, HMESSAGEREAD hDataMessage);
+		virtual DRESULT			WriteToMessageHMessageRead(HMESSAGEWRITE hMessage, HMESSAGEREAD hDataMessage) = 0;
 		
 		// Helper so you don't have to FormatString and FreeString..
-		virtual DRESULT			WriteToMessageFormattedHString(HMESSAGEWRITE hMessage, int messageCode, ...);
+		virtual DRESULT			WriteToMessageFormattedHString(HMESSAGEWRITE hMessage, int messageCode, ...) = 0;
 
 		// Note: you can't send object references to the client yet, so the client can't
 		// even read object references.
 		// You can't write object references in a HMESSAGEWRITE passed in MID_SAVEOBJECT.
-		virtual DRESULT			WriteToMessageObject(HMESSAGEWRITE hMessage, HOBJECT hObj);	
+		virtual DRESULT			WriteToMessageObject(HMESSAGEWRITE hMessage, HOBJECT hObj) = 0;
 
 		// Use this only while saving objects (inside MID_SAVEOBJECT).
-		virtual DRESULT			WriteToLoadSaveMessageObject(HMESSAGEWRITE hMessage, HOBJECT hObject);
+		virtual DRESULT			WriteToLoadSaveMessageObject(HMESSAGEWRITE hMessage, HOBJECT hObject) = 0;
 		
 
 		// When your OnMessage function is called, use the handle you're given
 		// to read the message data with these functions.
-		virtual float			ReadFromMessageFloat(HMESSAGEREAD hMessage);
-		virtual DBYTE			ReadFromMessageByte(HMESSAGEREAD hMessage);
-		virtual D_WORD			ReadFromMessageWord(HMESSAGEREAD hMessage);
-		virtual DDWORD			ReadFromMessageDWord(HMESSAGEREAD hMessage);
-		virtual char*			ReadFromMessageString(HMESSAGEREAD hMessage);
-		virtual void			ReadFromMessageVector(HMESSAGEREAD hMessage, DVector *pVal); // 12 bytes
-		virtual void			ReadFromMessageCompVector(HMESSAGEREAD hMessage, DVector *pVal); // 9 bytes
-		virtual void			ReadFromMessageCompPosition(HMESSAGEREAD hMessage, DVector *pVal); // 7 bytes
-		virtual void			ReadFromMessageRotation(HMESSAGEREAD hMessage, DRotation *pVal);
-		virtual HOBJECT			ReadFromMessageObject(HMESSAGEREAD hMessage);
-		virtual HSTRING			ReadFromMessageHString(HMESSAGEREAD hMessage);
+		virtual float			ReadFromMessageFloat(HMESSAGEREAD hMessage) = 0;
+		virtual DBYTE			ReadFromMessageByte(HMESSAGEREAD hMessage) = 0;
+		virtual D_WORD			ReadFromMessageWord(HMESSAGEREAD hMessage) = 0;
+		virtual DDWORD			ReadFromMessageDWord(HMESSAGEREAD hMessage) = 0;
+		virtual char*			ReadFromMessageString(HMESSAGEREAD hMessage) = 0;
+		virtual void			ReadFromMessageVector(HMESSAGEREAD hMessage, DVector *pVal) = 0; // 12 bytes
+		virtual void			ReadFromMessageCompVector(HMESSAGEREAD hMessage, DVector *pVal) = 0; // 9 bytes
+		virtual void			ReadFromMessageCompPosition(HMESSAGEREAD hMessage, DVector *pVal) = 0; // 7 bytes
+		virtual void			ReadFromMessageRotation(HMESSAGEREAD hMessage, DRotation *pVal) = 0;
+		virtual HOBJECT			ReadFromMessageObject(HMESSAGEREAD hMessage) = 0;
+		virtual HSTRING			ReadFromMessageHString(HMESSAGEREAD hMessage) = 0;
 		
 		// Use this only while loading objects (inside MID_LOADOBJECT).
-		virtual DRESULT			ReadFromLoadSaveMessageObject(HMESSAGEREAD hMessage, HOBJECT *hObject);
+		virtual DRESULT			ReadFromLoadSaveMessageObject(HMESSAGEREAD hMessage, HOBJECT *hObject) = 0;
 
 		// Reads a data message from an HMESSAGEREAD.  The returned HMESSAGEREAD can then be used in the
 		// ReadFromMessageX functions.  This will create a new HMESSAGEREAD which must be
 		// freed with a call to EndHMessageRead().
 		// Inputs:
 		//		hMessage -		HMESSAGEREAD read from.
-		virtual HMESSAGEREAD	ReadFromMessageHMessageRead(HMESSAGEREAD hMessage);
+		virtual HMESSAGEREAD	ReadFromMessageHMessageRead(HMESSAGEREAD hMessage) = 0;
 
 		// Frees a HMESSAGEREAD created from a call of ReadFromMessageHMessageRead.
-		virtual void			EndHMessageRead(HMESSAGEREAD hMessage);
+		virtual void			EndHMessageRead(HMESSAGEREAD hMessage) = 0;
 
 		// Frees a HMESSAGEWRITE created from a call of StartHMessageWrite.
-		virtual void			EndHMessageWrite(HMESSAGEWRITE hMessage);
+		virtual void			EndHMessageWrite(HMESSAGEWRITE hMessage) = 0;
 		
 		// Reset reading (so you can read the message again).
 		// This is useful if you read out of a message and subclasses
 		// will be reading out of it.  Note: the message will AUTOMATICALLY
 		// reset when you hit the end, so you won't need this in most cases.
-		virtual void			ResetRead(HMESSAGEREAD hRead);
+		virtual void			ResetRead(HMESSAGEREAD hRead) = 0;
 	};
 
 
