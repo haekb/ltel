f6f2eec19102f9486b01e9e97870195be17eb295
Jake <haekbreen@gmail.com>
2020-11-09 19:05:20 -0800

    Add a "profile" option to our game.cfg, so I don't have to keep swapping section names!

--- GodotProj/src/game_config.gd
+++ GodotProj/src/game_config.gd
@@ -10,8 +10,16 @@ func load_config():
 	if err != OK:
 		print("Couldn't find game.cfg!")
 		return false
+		
+	# Grab the profile
+	var profile = config.get_value("Game", "profile", "Game_Shogo")
 	
-	self.game_name = config.get_value("Game", "name", self.game_name)
-	self.game_data_dir = config.get_value("Game", "data_dir", self.game_data_dir)
+	print("[GameConfig] Loading game profile %s" % profile)
+	
+	self.game_name = config.get_value(profile, "name", self.game_name)
+	self.game_data_dir = config.get_value(profile, "data_dir", self.game_data_dir)
+	
+	print("[GameConfig] Found config for %s, data directory: %s" % [self.game_name, self.game_data_dir])
+	
 	return true
 # End Func
